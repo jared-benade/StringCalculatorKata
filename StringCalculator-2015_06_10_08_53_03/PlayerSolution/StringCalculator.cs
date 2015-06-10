3ec46c61d5411ea45cd49e4bd0da72f1b9fa60cc
unknown <Jared@Jared.chilli.local>
2015-06-10 09:58:39 +0200

    Added first attempt at string calculator kata

---
+++ StringCalculator-2015_06_10_08_53_03/PlayerSolution/StringCalculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using Katarai.StringCalculator.Interfaces;
+
+namespace PlayerStringKata
+{
+    public class StringCalculator : IStringCalculator
+    {
+        public int Add(string input)
+        {
+            if (string.IsNullOrEmpty(input)) return 0;
+
+            var seperatedValues = new[] {""};
+
+            if (input.Contains(",") || input.Contains("\n")) seperatedValues = input.Split(new Char[] { ',', '\n' });
+
+            if (string.IsNullOrEmpty(seperatedValues.First())) return int.Parse(input);
+            
+            return seperatedValues.Sum(number => int.Parse(number));
+        }
+    }
+}
---
+++ StringCalculator-2015_06_10_08_53_03/PlayerSolution/TestStringCalculator.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Engine;
+using Katarai.StringCalculator.Interfaces;
+using NUnit.Framework;
+
+namespace PlayerStringKata
+{
+    public class TestStringCalculator : ITestPack<IStringCalculator>
+    {
+        public Func<IStringCalculator> CreateSUT { get; set; }
+
+		[SetUp]
+        public void SetupTest()
+        {
+            CreateSUT = () => new StringCalculator();
+        }
+		
+		private IStringCalculator CreateCalculator()
+		{
+			return CreateSUT();
+		}
+		
+        /// <summary>
+        /// This is a sample test that shows how the StringCalculator 
+        /// should be created in future tests. 
+        /// </summary>
+        [Test]
+        public void Constructor()
+        {
+            //---------------Arrange-------------------
+            
+            //---------------Act----------------------
+            var calculator = CreateCalculator();
+            //---------------Assert-----------------------
+            Assert.IsNotNull(calculator);
+        }
+
+        [Test]
+        public void Add_GivenInput_EmptyString_ShouldReturn_0()
+        {
+            const int expected = 0;
+
+            var actual = CreateCalculator().Add("");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Add_GivenInput_NumericString_ShouldReturn_IntegarValue()
+        {
+            const int expected = 1;
+
+            var actual = CreateCalculator().Add("1");
+
+            Assert.AreEqual(expected,actual);
+        }
+
+        [Test]
+        public void Add_GivenInput_NumericStringOf2NumbersSeperatedByCommas_ShouldReturn_SumOfValues()
+        {
+            const int expected = 3;
+
+            var actual = CreateCalculator().Add("1,2");
+
+            Assert.AreEqual(expected,actual);
+        }
+
+        [Test]
+        public void Add_GivenInput_NumericStringOf3NumbersSeperatedByCommas_ShouldReturn_SumOfValues()
+        {
+            const int expected = 6;
+
+            var actual = CreateCalculator().Add("1,2,3");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Add_GivenInput_NumericStringOf2NumbersSeperatedByNewLine_ShouldReturn_SumOfValues()
+        {
+            const int expected = 3;
+
+            var actual = CreateCalculator().Add("1\n2");
+
+            Assert.AreEqual(expected,actual);
+        }
+
+        [Test]
+        public void Add_GivenInput_NumericStringOf3NumbersSeperatedByNewLine_ShouldReturn_SumOfValues()
+        {
+            const int expected = 10;
+
+            var actual = CreateCalculator().Add("5\n4\n1");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Add_GivenInput_NumericStringOf3NumbersSeperatedByNewLineAndCommas_ShouldReturn_SumOfValues()
+        {
+            const int expected = 11;
+
+            var actual = CreateCalculator().Add("5\n5,1");
+
+            Assert.AreEqual(expected,actual);
+        }
+      
+    }
+}
